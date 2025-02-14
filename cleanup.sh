c150dda7134ba107d75dafa4e4e7cf0157e8d1c8
Hocheol Choi <show1693@gmail.com>
2025-02-14 14:28:03 +0900

    feat: Fix error

---
+++ cleanup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$RUNNER_ALLOW_RUNASROOT" != "1" ]; then
+	echo "Error: RUNNER_ALLOW_RUNASROOT is not set to 1."
+	exit 1
+fi
+
+if [ -z "$PAT" ]; then
+	echo "Error: PAT is not set."
+	exit 1
+fi
+
+if [ -z "$ORG" ]; then
+	echo "Error: ORG is not set."
+	exit 1
+fi
+
+cleanup() {
+	runner_ids=$(
+		curl -s -L \
+			-H "Accept: application/vnd.github+json" \
+			-H "Authorization: Bearer ${PAT}" \
+			-H "X-GitHub-Api-Version: 2022-11-28" \
+			https://api.github.com/orgs/${ORG}/actions/runners \
+			| jq -r '.runners[].id'
+	)
+
+	runner_len=$(echo "$runner_ids" | wc -l)
+	echo "Detected $runner_len runners."
+
+	if [ $runner_len -ge 2 ]; then
+		for runner_id in $runner_ids; do
+			curl -s -L \
+				-X DELETE \
+				-H "Accept: application/vnd.github+json" \
+				-H "Authorization: Bearer ${PAT}" \
+				-H "X-GitHub-Api-Version: 2022-11-28" \
+				https://api.github.com/orgs/${ORG}/actions/runners/${runner_id}
+		done
+
+		echo "Cleaned up $runner_len runners."
+	fi
+}
+
+echo "Trying to cleanup runners..."
+cleanup
--- start.sh
+++ start.sh
@@ -31,35 +31,5 @@ run() {
 	./run.sh
 }
 
-cleanup() {
-	runner_ids=$(
-		curl -s -L \
-			-H "Accept: application/vnd.github+json" \
-			-H "Authorization: Bearer ${PAT}" \
-			-H "X-GitHub-Api-Version: 2022-11-28" \
-			https://api.github.com/orgs/${ORG}/actions/runners \
-			| jq -r '.runners[].id'
-	)
-
-	runner_len=$(echo "$runner_ids" | wc -l)
-	echo "Detected $runner_len runners."
-
-	if [ $runner_len -ge 2 ]; then
-		for runner_id in $runner_ids; do
-			curl -s -L \
-				-X DELETE \
-				-H "Accept: application/vnd.github+json" \
-				-H "Authorization: Bearer ${PAT}" \
-				-H "X-GitHub-Api-Version: 2022-11-28" \
-				https://api.github.com/orgs/${ORG}/actions/runners/${runner_id}
-		done
-
-		echo "Cleaned up $runner_len runners."
-	fi
-}
-
-echo "Trying to cleanup runners..."
-cleanup
-
 echo "Starting runner..."
 run
